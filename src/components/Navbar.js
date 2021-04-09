d34937141544096cb9341326709b88dc9691de1c
Jordan Long <jblong27@gmail.com>
2021-04-09 12:34:35 -0500

    added new dropdown button and made it white

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import logo from '../logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
@@ -7,8 +9,9 @@ const Navbar = () => {
       <div className="container">
 
         <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
