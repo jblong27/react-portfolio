d097094360c7427fbb69bcdef5b6c0ee1fab36d6
Jordan Long <jblong27@gmail.com>
2021-04-22 11:52:53 -0500

    change to about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -15,12 +15,11 @@ const AboutMe = () => {
           <div className="col-lg-6 col-xm-12">
             <h1 className="about-heading">About Me</h1>
               <p>
-              I am a software developer from Nashville, TN with experience in HTML, CSS, JS, React, MongoDB, SQL and Node.js. 
+                I am a software developer from Nashville, TN with experience in HTML, CSS, JS, React, MongoDB, SQL and Node.js. 
               My background is in logistics so I have a lot of experience working in a team setting, dealing with people and problem solving.
-              I also would describe myself as a tinkerer, I like trying to make things work and working with all kinds of technology. 
-              I built my first desktop computer in middle school and have been doing new builds ever since. 
               I have always been interested in programming but never took the full leap until 2020 and that's been the only thing I've been focused on since. 
-              So now I'm just looking to get my start somewhere and learn even more.
+              Now, with each new project, my aim is to provide the user with a clean and easy to use interface that is fast loading and responsive.
+              I’m excited to use my skills as part of a fast-paced, quality-driven team to build better experiences on the web. 
               </p>
           </div>
         </div>
